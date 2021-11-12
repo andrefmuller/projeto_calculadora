4c0d12897f8b5446b016bceac432a4b985770052
André Frederico Müller <andrefredericomuller@gmail.com>
2021-11-11 22:02:10 -0300

    Calculadora

---
+++ calculadora.js
@@ -0,0 +1,55 @@
+function botao1(){
+
+}
+
+function botao2(){
+
+}
+
+function botao3(){
+
+}
+
+function botao4(){
+
+}
+
+function botao5(){
+
+}
+
+function botao6(){
+
+}
+
+function botao7(){
+
+}
+
+function botao8(){
+
+}
+
+function botao9(){
+
+}
+
+function botao0(){
+
+}
+
+function botao_mais(){
+
+}
+
+function botao_menos(){
+
+}
+
+function botao_vezes(){
+
+}
+
+function botao_divide(){
+    
+}
